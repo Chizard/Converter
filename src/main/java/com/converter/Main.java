0cbc00a1f06ce5288638ba02ca87b8a804f8ec38
Adept <engmantobias@gmail.com>
2022-08-07 00:52:07 +0200

    made it look nicer

--- src/main/java/com/converter/Main.java
+++ src/main/java/com/converter/Main.java
@@ -17,23 +17,38 @@ public class Main {
 		float ans2;
 		float ans3;
 		int answer;
+		boolean active = true;
+
+	while (true) {
+
+
+		System.out.println("1. Convert Fahrenheit to Celsius \n" +
+				           "2. Convert Celsius to Fahrenheit\n" +
+				           "3. Quit");
+		answer = scan.nextInt();
+
+		if (answer == 3) {
+			System.exit(0);
+		}
 
 		System.out.println("Enter the value you want to convert: ");
 		ans = scan.nextFloat();
-		System.out.println("1. Convert Fahrenheit to Celsius \n" +
-				"2. Convert Celsius to Fahrenheit");
-		answer = scan.nextInt();
 
 		if (answer == 1) {
 		ans2 = (float) (ans-32);
 		ans3 = (float) (ans2*(5)/9);
-
+		System.out.println("-------------------------------------");
 		System.out.println(ans + " Fahrenheit is " + ans3 + " Celsius");
+		System.out.println("-------------------------------------");
 		}
+
 		if (answer == 2) {
 			ans1 = (float) ((ans*(1.8))+32);
+			System.out.println("-------------------------------------");
 			System.out.println(ans + " Celsius is " + ans1 + " Fahrenheit");
+			System.out.println("-------------------------------------");
 		}
 
+	}
 	}
 }
